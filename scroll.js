7c2e529189d62ef0633102d577f7341b103af847
Teo <th3od0ry@gmail.com>
2023-05-09 22:49:47 +0300

    nav animation and buttons

--- scroll.js
+++ scroll.js
@@ -1,6 +1,7 @@
 const imagine = document.getElementById('headerImg');
 const nav = document.getElementById('navigare');
 const navTitle = document.querySelector('nav h2');
+const menuImg = document.getElementById('menuImg');
 
 window.addEventListener('scroll', () => {
   const scrollTop = document.documentElement.scrollTop;
@@ -12,9 +13,11 @@ window.addEventListener('scroll', () => {
   if (scrollPercentage*100 > 39) {
     nav.style.opacity = '1';
     navTitle.style.opacity = '1';
+    menuImg.style.opacity = '1';
   } else {
     nav.style.opacity = '0';
     navTitle.style.opacity = '0';
+    menuImg.style.opacity = '0';
   }
     
 });
