d564c755dc1216364e91bcab4b88ce7b6e00b647
Teo <th3od0ry@gmail.com>
2023-05-22 18:59:08 +0300

    hptos

--- cardshow.js
+++ cardshow.js
@@ -1,63 +1,28 @@
-let cnt = 1;
-const left = document.getElementById('arrowLeft');
-const right = document.getElementById('arrowRight');
-const cards = [
-  document.getElementById('card0'),
-  document.getElementById('card1'),
-  document.getElementById('card2')
-];
+var currentCardIndex = 0;
+  var cards = document.getElementsByClassName("card");
+  var indicators = document.getElementsByClassName("indicator");
 
-let cnt1 = 0; // Initialize the cnt variable to 0
-const indicators = [
-  document.getElementById('indicator0'),
-  document.getElementById('indicator1'),
-  document.getElementById('indicator2')
-];
-
-function updateIndicators() {
-  for (let i = 0; i < indicators.length; i++) {
-    indicators[i].classList.remove('active');
-  }
-  indicators[cnt1].classList.add('active');
-}
-
-function cardSwapRight() {
-  const lastCard = cards[cards.length - 1];
-  cards.pop(); // Remove the last card from the array
-  cards.unshift(lastCard); // Insert the last card at the beginning of the array
-
-  // Update the positions of the cards
-  if (cnt !== 2) {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].style.transform = `translateX(100%)`;
+  function showCard(index) {
+    if (index >= 0 && index < cards.length) {
+      for (var i = 0; i < cards.length; i++) {
+        cards[i].style.display = "none";
+        indicators[i].classList.remove("active");
+      }
+      cards[index].style.display = "block";
+      indicators[index].classList.add("active");
+      currentCardIndex = index;
     }
-    cnt = 2;
-  } else {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].style.transform = `translateX(0%)`;
-    }
-    cnt = 1;
   }
-  updateIndicators();
-}
 
-function cardSwapLeft() {
-  const firstCard = cards.shift(); // Remove the first card from the array
-  cards.push(firstCard); // Insert the first card at the end of the array
-
-  // Update the positions of the cards
-  if (cnt != 2) {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].style.transform = `translateX(${-100}%)`;
-    }
-    cnt = 2;
+  function cardSwapLeft() {
+    var newIndex = (currentCardIndex - 1 + cards.length) % cards.length;
+    showCard(newIndex);
   }
-  else {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].style.transform = `translateX(${0}%)`;
-    }
-    cnt = 1;
-  }
-  updateIndicators();
-}
 
+  function cardSwapRight() {
+    var newIndex = (currentCardIndex + 1) % cards.length;
+    showCard(newIndex);
+  }
+
+  // Show the initial card
+  showCard(currentCardIndex);
